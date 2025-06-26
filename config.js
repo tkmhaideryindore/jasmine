dde28187027c7a41939d244ed52bbc314180fce7
tkmindore <tkmhaideryindore@gmail.com>
2025-06-26 18:04:18 +0530

    Update config.js

--- config.js
+++ config.js
@@ -1,7 +1,7 @@
 // Configuration - Keep this file secure and don't expose in production
 const CONFIG = {
   // Base64 encoded Google Sheets URL to make it less obvious in browser inspector
-  SHEET_URL: 'aHR0cHM6Ly9kb2NzLmdvb2dsZS5jb20vc3ByZWFkc2hlZXRzL2QvMTdncm4wa09DcjVRVU50RUVHbkxPS1VhUlhKRjAzY0V2ZTBzeFR1WTBGRmsvZXhwb3J0P2Zvcm1hdD1jc3YmZ2lkPTA='
+  SHEET_URL: 'aHR0cHM6Ly9kb2NzLmdvb2dsZS5jb20vc3ByZWFkc2hlZXRzL2QvMW5LNHFELTdvOWtJSHpOZjVISjNyeExiRVNuWExpcE04VEdJQVl2ak9iZmcvZWRpdD9naWQ9MCNnaWQ9MA=='
 };
 
 // Decode function
